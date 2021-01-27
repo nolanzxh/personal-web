9d0b2a53a3f7b9441255861fbe2333d7f5808879
nolan <nolan_zxh@sina.com>
2021-01-28 00:12:20 +0100

    Alleviate menu component

--- src/components/MenuItem/MenuItem.js
+++ src/components/MenuItem/MenuItem.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import "./MenuItem.css";
 import { withRouter } from 'react-router-dom';
-import { motion } from "framer-motion";
 
 class MenuItem extends React.Component {
 
@@ -16,28 +15,17 @@ class MenuItem extends React.Component {
     render() {
         const { isSmallScreen, active } = this.props;
         return (
-            <motion.div
-                className={isSmallScreen ? 'menuitem_small' : 'menuitem'}
-                initial='rest'
-                whileHover='hover'
-            >
-                <motion.div
+            <div
+                className={isSmallScreen ? 'menuitem_small' : 'menuitem'}>
+                <div
                     className={`${isSmallScreen ? 'icon_button_small' : 'icon_button'} ${active ? 'menuitem_active' : null}`}
                     onClick={this.onPageChange}
-                    variants={isSmallScreen ? {
-                        rest: { color: '#a9a9a9' },
-                        hover: {
-                            color: '#fff',
-                            transition: .3
-                        }
-                    } : null}
                 >
                     <span className="iconfont menu_icon">{this.props.icon}</span>
-                </motion.div>
-                <motion.div
-                    className={isSmallScreen ? 'itemname_small' : 'itemname'}
-                >{this.props.itemName}</motion.div>
-            </motion.div>
+                </div>
+                <div
+                    className={isSmallScreen ? 'itemname_small' : 'itemname'}>{this.props.itemName}</div>
+            </div>
         )
     }
 }
